bba2b46a60047a1ef81238a7f06226d0e1535e3a
hopehappy92 <hopehappy92@naver.com>
2021-08-20 14:05:38 +0900

    Add : file input (#2)

--- src/components/form/input/other-input/FileInput.stories.tsx
+++ src/components/form/input/other-input/FileInput.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import FileInput, { FileInputProps } from './FileInput';
@@ -8,14 +8,87 @@ export default {
   component: FileInput,
   args: {
     theme: 'default'
+  },
+  argTypes: {
+    text: { control: 'text' },
+    accept: { control: 'text' },
+    isMulti: { control: 'boolean' }
   }
 } as Meta;
 
 const Template: Story<FileInputProps> = (args: FileInputProps) => {
-  const { theme } = args;
+  const {
+    theme,
+    text,
+    accept,
+    isMulti,
+    uploadedFileComponent,
+    onUploadFiles
+  } = args;
 
-  return <FileInput theme={theme} />;
+  return (
+    <FileInput
+      theme={theme}
+      text={text}
+      accept={accept}
+      isMulti={isMulti}
+      uploadedFileComponent={uploadedFileComponent}
+      onUploadFiles={onUploadFiles}
+    />
+  );
 };
 
 export const FileInputDefault = Template.bind({});
 FileInputDefault.args = {};
+
+export const FileInputMulti = Template.bind({});
+FileInputMulti.args = {
+  isMulti: true
+};
+
+export const FileInputWithComponent: Story<FileInputProps> = (
+  args: FileInputProps
+) => {
+  const {
+    theme,
+    text,
+    accept,
+    isMulti
+    // uploadedFileComponent,
+    // onUploadFiles
+  } = args;
+
+  const myCompStyle = {
+    fontSize: '12px',
+    margin: '10px'
+  };
+
+  const [files, setFiles] = useState<FileList | null>(null);
+
+  const upload = (uploadedFiles: FileList | null) => {
+    setFiles(uploadedFiles);
+  };
+
+  const myComponent = (
+    <div style={myCompStyle}>
+      {files && (
+        <>
+          <p style={{ margin: '0 0 5px 0' }}>Uploaded File</p>
+          <span>{files[0].name}</span>
+        </>
+      )}
+    </div>
+  );
+
+  return (
+    <FileInput
+      theme={theme}
+      text={text}
+      accept={accept}
+      isMulti={isMulti}
+      uploadedFileComponent={myComponent}
+      onUploadFiles={upload}
+    />
+  );
+};
+FileInputWithComponent.args = {};
--- src/components/form/input/other-input/FileInput.tsx
+++ src/components/form/input/other-input/FileInput.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import bytes from 'bytes';
 
 import './FileInputStyle.scss';
 
@@ -8,16 +9,108 @@ type TTheme = 'default' | 'info' | 'secondary' | 'warning' | 'danger';
 export interface FileInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   theme?: TTheme;
+  text?: string;
+  accept?: string;
+  isMulti?: boolean;
+  uploadedFileComponent?: React.ReactNode;
+  onUploadFiles?: (files: FileList | null) => void;
 }
 
 const FileInput: React.FC<FileInputProps> = ({
   theme = 'default',
-
+  text = 'Click me Or Drop me',
+  accept,
+  isMulti = false,
+  uploadedFileComponent,
+  onUploadFiles,
   ...props
 }: FileInputProps) => {
   const hash = uuid().split('-')[0];
 
-  return <div className={[`muop-file-input-wrapper`].join(' ')}>asdf</div>;
+  const [uploadedFiles, setUploadedFiles] = useState<File[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const labelRef = useRef<HTMLLabelElement>(null);
+
+  const convertFileListToArray = useCallback((fileList: FileList | null) => {
+    const files: FileList | null = Object(fileList);
+    const tempFileList: File[] = [];
+    if (files) {
+      for (let i = 0; i < files.length; i += 1) {
+        tempFileList.push(files[i] as File);
+      }
+      setUploadedFiles(tempFileList);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (inputRef.current && labelRef.current) {
+      const { width, height } = labelRef.current.getClientRects()[0];
+      inputRef.current.style.width = `${width}px`;
+      inputRef.current.style.height = `${height}px`;
+    }
+  }, [uploadedFiles]);
+
+  const onFilesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    convertFileListToArray(e.currentTarget.files);
+    onUploadFiles?.(e.currentTarget.files);
+  };
+
+  const onFilesResetBtnClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.value = '';
+    setUploadedFiles(null);
+    onUploadFiles?.(null);
+  };
+
+  const onDropFiles = (e: React.DragEvent<HTMLInputElement>) => {
+    convertFileListToArray(e.currentTarget.files);
+    onUploadFiles?.(e.currentTarget.files);
+  };
+
+  return (
+    <div
+      className={[
+        `muop-file-input-wrapper`,
+        `muop-file-input-wrapper-${theme}`
+      ].join(' ')}
+    >
+      <input
+        id={`muop-file-input-${hash}`}
+        ref={inputRef}
+        type="file"
+        multiple={isMulti}
+        accept={accept}
+        onChange={onFilesChange}
+        onClick={onFilesResetBtnClick}
+        onDrop={onDropFiles}
+        {...props}
+      />
+      <label
+        ref={labelRef}
+        htmlFor={`muop-file-input-${hash}`}
+        className={[
+          `${
+            uploadedFiles && !uploadedFileComponent
+              ? 'muop-file-input-label-span'
+              : ''
+          }`,
+          `${!uploadedFiles ? 'muop-file-input-label-default' : ''}`
+        ].join(' ')}
+      >
+        {!uploadedFiles && text}
+        {uploadedFiles &&
+          (!uploadedFileComponent
+            ? uploadedFiles?.map(file => (
+                <span key={`uploaded-file-${file.name}`}>
+                  <span>{file.name}</span>
+                  <span>{bytes(file.size)}</span>
+                </span>
+              ))
+            : uploadedFileComponent)}
+      </label>
+      {}
+    </div>
+  );
 };
 
 FileInput.displayName = 'FileInput';
